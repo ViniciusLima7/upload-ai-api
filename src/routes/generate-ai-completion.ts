cb79c43b04b687721a5b414dd93d0ff3fec035b1
Vinicius Lima <marcos.vf@samsung.com>
2023-09-14 20:55:14 -0300

    feat: create router generate ai completetion

--- src/routes/generate-ai-completion.ts
+++ src/routes/generate-ai-completion.ts
@@ -5,7 +5,7 @@ import { prisma } from "../lib/prisma";
 import { openai } from "../lib/openai";
 
 export async function generateAICompletionRoute(app: FastifyInstance) {
-  app.post("/ai/complete", async (req) => {
+  app.post("/ai/complete", async (req, res) => {
     const bodySchema = z.object({
       videoId: z.string().uuid(),
       template: z.string(),
@@ -14,10 +14,29 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
 
     const { videoId, temperature, template } = bodySchema.parse(req.body);
 
-    return {
-      videoId,
-      template,
+    const video = await prisma.video.findUniqueOrThrow({
+      where: {
+        id: videoId,
+      },
+    });
+
+    if (!video.transcription) {
+      return res
+        .status(400)
+        .send({ error: "Video Transcription was not generated" });
+    }
+
+    const promptMessage = template.replace(
+      "{transcription}",
+      video.transcription
+    );
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo-16k",
       temperature,
-    };
+      messages: [{ role: "user", content: promptMessage }],
+    });
+
+    return response;
   });
 }
